4a14689d34d63cac682df95fc86c071b33184447
Farley Guimarães <farley_guimaraes@hotmail.com>
2021-10-07 08:45:41 -0300

    Adicionando sprite da cabeça da cobrinha

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ sprites.src = "./snake-graphics.png";
 
 let canvas = document.getElementById("snake"); //criar elemento que irá rodar o jogo
 let context = canvas.getContext("2d"); //....
+
 let box = 32;
+
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 snake[0] ={
     x: 8 * box,
-    y: 8 * box
+    y: 8 * box,
+    /*direction: {
+      x: 1,
+      y:0
+    }*/
 }
+
 let direction = "right";
+
 let food ={
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
@@ -17,14 +25,24 @@ let food ={
 
 function criarBG(){
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16*box, 16*box); //desenha o retângulo usando x e y e a largura e altura setadas
+    context.fillRect(0, 0, 18*box, 18*box); //desenha o retângulo usando x e y e a largura e altura setadas
 }
 
 function criarCobrinha (){
-    for(i = 0; i < snake.length; i++){
+
+    context.drawImage(
+        sprites,
+        254, 0,
+        64, 64,
+        snake[0].x, snake[0].y,
+        box, box
+    );
+
+    for (i = 1; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+
 }
 
 function drawFood (){
@@ -48,6 +66,15 @@ function update(event){
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
     if(event.keyCode == 39 && direction != 'left') direction = 'right';
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
+
+    /*if(event.keyCode == 37 && snake[0].direction.x != 1)
+       snake[0].direction = { x : -1 , y : 0 }; //left  
+    if(event.keyCode == 38 && snake[0].direction.y != 1)
+       snake[0].direction = { x : 0 , y : -1 }; //up  
+    if(event.keyCode == 39 && snake[0].direction.x != -1) 
+       snake[0].direction = { x : 1 , y : 0 }; //right  
+    if(event.keyCode == 40 && snake[0].direction.y != -1)
+       snake[0].direction = { x : 0 , y : 1 }; //down*/
 }
 
 function iniciarJogo(){    
